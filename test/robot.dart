f6880068bcdb9981d8422447265842e0e4eaa25f
Ryoichi Izumita <r_izumita@yumemi.co.jp>
2023-06-12 14:35:39 +0900

    Robotをabstractに変更

--- test/robot.dart
+++ test/robot.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:page_object/page_object.dart';
 
-class Robot<W extends Widget> extends PageObject {
+abstract class Robot<W extends Widget> extends PageObject {
   Robot(this.tester) : super(find.byType(W));
 
   Robot.child(this.tester, parent)
